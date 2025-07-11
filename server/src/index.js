1228cd8cf10ec5b8c5177c7caf277be4e253d856
vivekgupta2004 <130595295+vivekgupta2004@users.noreply.github.com>
2025-07-12 01:07:13 +0530

    some change

--- server/src/index.js
+++ server/src/index.js
@@ -1,54 +1,42 @@
-import express from 'express';
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import path from 'path'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+import path from "path";
 
-import authRoutes from './routes/auth.route.js'
-import { connectDB } from './lib/db.js';
-import messageRoutes from './routes/message.route.js'
+import { connectDB } from "./lib/db.js";
 
-import cors from 'cors'
-import { app,server } from './lib/socket.js';
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
+dotenv.config();
 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-dotenv.config()
-
-
-
-
-const PORT = process.env.PORT
-const __dirname = path.resolve()
-
-
-app.use(cors({
-    origin: 'http://localhost:5173',  // Allow frontend origin
-    credentials: true                // Allow cookies, if you're using them
-}));
-
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
-
-
+app.use(express.json());
 app.use(cookieParser());
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../client/dist")))
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../client", "dist", "index.html"));
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-
-connectDB().then(() => {
-    server.listen(PORT, () => {
-        console.log("Server is running on port: " + PORT);
-    });
-}).catch((err) => {
-    console.error("❌ Failed to connect to MongoDB:", err.message);
-    process.exit(1); // exit with failure
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
