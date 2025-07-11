4d867ab09e29473fba30ec377a69cfe2b859b331
vivekgupta2004 <130595295+vivekgupta2004@users.noreply.github.com>
2025-07-12 00:27:37 +0530

    deployemnt setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-
+import path from 'path'
 
 import authRoutes from './routes/auth.route.js'
 import { connectDB } from './lib/db.js';
@@ -18,6 +18,8 @@ dotenv.config()
 
 
 const PORT = process.env.PORT
+const __dirname = path.resolve
+
 
 app.use(cors({
     origin: 'http://localhost:5173',  // Allow frontend origin
@@ -32,7 +34,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../client/dist")))
 
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../client", "dist", "index.html"));
+    })
+}
 
 
 connectDB().then(() => {
